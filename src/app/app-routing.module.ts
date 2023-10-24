dfce919f08aab36af58223c18bb812d1e5be0958
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-10-23 23:38:16 -0500

    Actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ContentComponent } from './content/content.component';
+import { ReservaComponent } from './reserva/reserva.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: 'profile',
-    component: LayoutComponent, // Use the layout component as the parent route
-  }
+    path: 'profile',component: LayoutComponent, // Use the layout component as the parent route 
+  },
+  { path: 'reserva', component: ReservaComponent}
 
 ];
 
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  isProfileRoute: boolean = false;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.isProfileRoute = this.route.snapshot.routeConfig?.path === 'profile';
+  }
 }
