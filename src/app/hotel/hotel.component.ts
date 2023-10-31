9db720ddeba6de89f43b44f2210221dc8ccdf52d
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-10-31 18:51:39 -0500

    Detalles de registro y eliminar un Hotel

--- src/app/hotel/hotel.component.ts
+++ src/app/hotel/hotel.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HotelserviceService } from '../service/hotelservice.service';
 import { TokenService } from '../service/token.service';
 import { Hotel } from '../../Entity/Hotel';
+import  Swal  from 'sweetalert2';
 
 @Component({
   selector: 'app-hotel',
@@ -9,6 +10,7 @@ import { Hotel } from '../../Entity/Hotel';
   styleUrls: ['./hotel.component.css'],
 })
 export class HotelComponent implements OnInit {
+  
   hotel: any[] = [];
   token: string | null = null;
   newHotel: Hotel = new Hotel(0, '', '', '', 0, '', new Date(), '');
@@ -33,7 +35,7 @@ export class HotelComponent implements OnInit {
   }
   }
 
-  addNewHotel() {
+ addNewHotel() {
     console.log(this.newHotel); 
     if (this.token) {
       this.axiosHotelService.addHotel(this.token, this.newHotel).subscribe(
@@ -41,27 +43,57 @@ export class HotelComponent implements OnInit {
           console.log('Nuevo hotel registrado:', response);
           this.hotel.push(response);
           this.newHotel = new Hotel(0, '', '', '', 0, '', new Date(), '');
+  
+          // Utilizando SweetAlert2 para mostrar un mensaje de confirmación
+          Swal.fire({
+            icon: 'success',
+            title: '¡Hotel añadido!',
+            showConfirmButton: false,
+            timer: 1500 // Opcional: tiempo en milisegundos antes de que se cierre el mensaje
+          });
         },
         (error) => {
           console.error('Error al registrar el nuevo hotel:', error);
+          // Mostrar un mensaje de error con SweetAlert2
+          Swal.fire({
+            icon: 'error',
+            title: 'Error al añadir el hotel',
+            text: 'Hubo un problema al registrar el hotel, por favor intenta de nuevo.'
+          });
         }
       );
+    }
+  }
+  
+  deleteHotel(hotelId: number) {
+    if (this.token !== null) {
+      // Mostrar el mensaje de confirmación con SweetAlert
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: '¡No podrás revertir esto!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Sí, ¡elimínalo!'
+      }).then((result) => {
+        if (result.isConfirmed) { // Verificar si se confirmó la eliminación
+          if (this.token !== null) { // Verificar nuevamente el token antes de hacer la llamada
+            this.axiosHotelService.deleteHotel(this.token, hotelId).subscribe(
+              (response) => {
+                console.log('Hotel eliminado ID:', hotelId);
+                // Remover el hotel eliminado de la lista
+                this.hotel = this.hotel.filter((h) => h.hotelId !== hotelId);
+              },
+              (error) => {
+                console.error('Error al eliminar el hotel:', error);
+              }
+            );
+          }
+        }
+      });
     }
   }
 
-  deleteHotel(hotelId: number) {
-    if (this.token) {
-      this.axiosHotelService.deleteHotel(this.token, hotelId).subscribe(
-        (response) => {
-          console.log('Hotel eliminado ID:', hotelId);
-          // Remover el hotel eliminado de la lista
-          this.hotel = this.hotel.filter((h) => h.hotelId !== hotelId);
-        },
-        (error) => {
-          console.error('Error al eliminar el hotel:', error);
-        }
-      );
-    }
-  }
- 
+  
 }
