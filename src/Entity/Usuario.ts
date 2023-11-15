a8a2edf73ef64074a30fb5db64fb3cad8bedc503
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-11-14 21:51:59 -0500

    Lista de usuarios, falta arreglar registrar y eliminar users por auth

--- src/Entity/Usuario.ts
+++ src/Entity/Usuario.ts
@@ -1,15 +1,25 @@
 export class User {
-    id: number;
-    email: string;
-    password: string;
-    username: string;
-    user: User[];
-  
-    constructor(id: number, email: string, password: string, username: string, user: User[]) {
-      this.id = id;
-      this.email = email;
-      this.password = password;
-      this.username = username;
-      this.user = user;
-    }
+  id: number;
+  email: string;
+  password: string;
+  username: string;
+  roles: Role[];
+
+  constructor(id: number, email: string, password: string, username: string, roles: Role[]) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+    this.username = username;
+    this.roles = roles;
   }
+}
+
+export class Role {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private ruta: string = 'http://localhost:8081';
+
+  constructor() { }
+
+  getAllUser(token: string): Observable<any> {
+    return new Observable<any>((observer) => {
+      axios
+        .get(`${this.ruta}/listUser`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          observer.next(response.data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+
+  public addUser(token: string, newUserData: any): Observable<any> {
+    return new Observable<any>((observer) => {
+      axios
+        .post(`${this.ruta}/createUser`, newUserData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          observer.next(response.data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+
+  deleteUser(token: string, userId: number): Observable<any> {
+    const url = `${this.ruta}/deleteUser?id=${userId}`;
+    return new Observable<any>((observer) => {
+      axios
+        .delete(url, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response) => {
+          observer.next(response.data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+  }
+
+}
